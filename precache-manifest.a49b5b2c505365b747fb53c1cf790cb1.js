5143a6cbe8e0834fb6417e8d66001e654d85c55d
Faran Navazi <faran.navazi@gmail.com>
2019-06-13 22:25:27 -0500

    Updates

---
+++ precache-manifest.a49b5b2c505365b747fb53c1cf790cb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b40df150a4295852d275af0079354d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f84485c6e755f9537240",
+    "url": "/clicky-game/static/css/main.d0ff3b80.chunk.css"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/clicky-game/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "f84485c6e755f9537240",
+    "url": "/clicky-game/static/js/main.42cfb9fb.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
